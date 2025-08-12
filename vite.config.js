19314abf00b9390bdd20f6042e97373dffaa88fe
pabloeorellana <pabloeorellana@gmail.com>
2025-08-12 10:49:58 -0300

    feat: AppJsxMod2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
-    }
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    // Vamos a ser lo más explícitos posible.
+    // Leemos la variable de entorno que Vercel *debería* proporcionar en el entorno de build
+    // y la asignamos a una variable global que nuestro código usará.
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
   }
-});
+})
